d5f2f8aefc567835135d03c7298a02e81932cb23
Salwa99 <s.ritage99@gmail.com>
2023-09-14 11:38:23 +0100

    Fetch the games

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import App from './App'
 import theme from './theme'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
