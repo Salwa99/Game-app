2ea01d4c9f89c5206f879a96e886d35f7aafbad6
Salwa99 <s.ritage99@gmail.com>
2023-09-15 14:57:06 +0100

    Define the loading state

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -25,21 +25,28 @@ const useGames = () => {
     
     const [games, setGames] = useState<Game[]>([]);
     const [error, setErrors] = useState('');
+    const [isLoading, setLoading] = useState(false);
     
     useEffect(() => {
       const controller = new AbortController();
 
+      setLoading(true);
+
       apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal })
-             .then(res => setGames(res.data.results))
+             .then(res => {setGames(res.data.results);
+              setLoading(false);
+            })
              .catch(err => {
                 if(err instanceof CanceledError) return;
-                setErrors(err.message)});
+                setErrors(err.message);
+                setLoading(false);
+            });
 
       return () => controller.abort()
 
   }, []);
 
-  return {games, error};
+  return {games, error, isLoading};
 
 }
 
