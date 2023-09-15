73f96cb6d910c5903671e89091e02de761a1d040
Salwa99 <s.ritage99@gmail.com>
2023-09-15 16:01:54 +0100

    Create a generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,7 +6,7 @@ import GameCardContainer from './GameCardContainer';
 
 const GameGrid = () => {
 
-    const { games, error, isLoading } = useGames();
+    const { data, error, isLoading } = useGames();
     const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
@@ -19,7 +19,7 @@ const GameGrid = () => {
              <GameCardSkeleton key={skeleton} />
           </GameCardContainer>
             )}
-            {games.map(game => 
+            {data.map(game => 
             <GameCardContainer>
               <GameCard key={game.id} game={game} />
             </GameCardContainer>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,4 @@
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 export interface Platform {
     id: number,
@@ -16,38 +14,6 @@ export interface Game {
     metacritic: number;
 }
 
-interface FetchGamesResponse {
-    count: number;
-    results: Game[]
-}
-
-const useGames = () => {
-    
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setErrors] = useState('');
-    const [isLoading, setLoading] = useState(false);
-    
-    useEffect(() => {
-      const controller = new AbortController();
-
-      setLoading(true);
-
-      apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal })
-             .then(res => {setGames(res.data.results);
-              setLoading(false);
-            })
-             .catch(err => {
-                if(err instanceof CanceledError) return;
-                setErrors(err.message);
-                setLoading(false);
-            });
-
-      return () => controller.abort()
-
-  }, []);
-
-  return {games, error, isLoading};
-
-}
+const useGames = () => useData<Game>('/games')
 
 export default useGames
