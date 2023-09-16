77d99d1511f992a6efbd84e5562775c6011c6d7f
Salwa99 <s.ritage99@gmail.com>
2023-09-16 13:43:47 +0100

    Fix the game cards style

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,7 +17,7 @@ const GameGrid = ({gameQuery}: Props) => {
   return (
     <>
         {error && <Text>{error}</Text>}
-        <SimpleGrid columns={{sm: 1, md: 2, lg: 3}} padding={10} spacing={3}>
+        <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} padding='10px' spacing={6}>
 
           {isLoading && skeletons.map((skeleton) => (
           <GameCardContainer key={skeleton}>
