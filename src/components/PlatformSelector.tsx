1fcc4fc58e33b37d4983219aae27f24d95346457
Salwa99 <s.ritage99@gmail.com>
2023-09-16 10:27:01 +0100

    Implement the game filter by platform

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -16,7 +16,7 @@ const PlatformSelector = ({onSelectPlatform, selectedPlatform}: Props) => {
 
   return (
     <Menu>
-        <MenuButton as={Button} rightIcon={<BsChevronDown />}>Platforms
+        <MenuButton as={Button} rightIcon={<BsChevronDown />}>{selectedPlatform?.name || 'Platforms'}
         </MenuButton>
         <MenuList>
              {data.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
